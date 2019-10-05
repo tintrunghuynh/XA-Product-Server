fee86025e03af87a63525a3b740c06e5625c325a
tintrunghuynh <tintrunghuynh.alviss@gmail.com>
2019-10-05 20:36:48 +0700

    auto-deoloy

---
+++ build/src/Graphql/message/error.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.errorMessage = {
+    required_name: "Status Is Required",
+    required_specificatoins: "Specification Is Required At Least 1 Not Null Field",
+    required_descriptions: "Descriptions Is Required",
+    required_status: "Status Is Required",
+    duplicated_name: "Name Already Exists"
+};
