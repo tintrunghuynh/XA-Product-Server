58258a7f5e28ad7f7e1c1e519763852dd270af79
tintrunghuynh <tintrunghuynh.alviss@gmail.com>
2019-11-03 20:45:50 +0700

    auto-deploy

--- build/src/app.js
+++ build/src/app.js
@@ -89,18 +89,15 @@ var Server = /** @class */ (function () {
         // connect to db
         var dbURI = process.env.MONGODB_URL;
         dbURI = "mongodb://adm:35351235@127.0.0.1:27017/XA-Product";
-        // dbURI = "mongodb+srv://CRUD:113355135@xa-product-dvtj7.mongodb.net/XA-Product?retryWrites=true&w=majority";
         var statusAutoIndex = true;
         if (process.env.NODE_ENV === "PROD") {
             // dbURI = process.env.MONGODB_P_URL;
             dbURI = "mongodb+srv://CRUD:113355135@xa-product-dvtj7.mongodb.net/XA-Product?retryWrites=true&w=majority";
             statusAutoIndex = false;
         }
-        console.log("dbURI: " + dbURI);
-        console.log(process.env.NODE_ENV);
         this.app.get("/", function (req, res, next) {
             // console.log("get");
-            res.send("\n            <h1> Welcome to Express with TS </h1>\n            <h2> Hosted by Heroku </h2>\n            <a href = \"/graphql-retrieve\" style = \"text-decoration:none; color: #33334d\" > Click to come to GraphiQL </a>            <p>dbURI: " + dbURI + "</p>\n            ");
+            res.send("\n            <h1> Welcome to Express with TS </h1>\n            <h2> Hosted by Heroku </h2>\n            <a href = \"/graphql-retrieve\" style = \"text-decoration:none; color: #33334d\" > Click to come to GraphiQL </a>\n            ");
         });
         mongoose_1.default.connect(Object(dbURI).toString(), {
             useNewUrlParser: true,
